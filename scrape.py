a04aa95af7e5adc97eb9f48dfef840bb6a02a029
user <aripoori111@gmail.com>
2022-11-19 17:37:42 -0500

    long list

--- scrape.py
+++ scrape.py
@@ -11,12 +11,27 @@ table = soup.find('tbody')
 
 locations = table.find_all('a')
 locationsList = []
+longlist = []
 for location in locations:
     if location.text != "report":
         locationsList.append(location.text)
+        longlist.append(location)
+    
 
 dates = table.find_all('span')
 datesList = []
 for date in dates:
     if date.text != "Felt" and date.text != "Not felt":
         datesList.append(date.text)
+for i in range (0,len(longlist)):
+    longlist[i]=str(longlist[i])
+    longlist[i]=longlist[i][9:]
+    longlist[i]=('https://earthquakescanada.nrcan.gc.ca') + longlist[i]
+    longlist[i].replace(str(locationsList[i]),'')
+    longlist[i]=longlist[i][:76]
+
+print(longlist)
+
+for i in range (0,len(longlist)):
+    html_text = requests.get(longlist[i]).text
+    soup = BeautifulSoup(html_text, 'lxml')
