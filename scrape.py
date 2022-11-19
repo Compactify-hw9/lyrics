102e83e35f5b4f9172034105153f2e701977b62c
Kevin Wu <kevwuhoo15@gmail.com>
2022-11-19 17:24:35 -0500

    formatting url

--- scrape.py
+++ scrape.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+year = 2022
+originalURL = 'https://earthquakescanada.nrcan.gc.ca/recent/2022/index-en.php#wb-auto-5'
+newURL = originalURL[0:45] + str(year) + originalURL[-23:]
 
 html_text = requests.get('https://earthquakescanada.nrcan.gc.ca/recent/2022/index-en.php#wb-auto-5').text
 soup = BeautifulSoup(html_text, 'lxml')
