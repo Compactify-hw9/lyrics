fb5810780c62b95ea66ce219042c0f54d06634bd
Kevin Wu <kevwuhoo15@gmail.com>
2022-11-19 16:47:07 -0500

    scraped locations and dates

---
+++ scrape.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+
+html_text = requests.get('https://earthquakescanada.nrcan.gc.ca/recent/2022/index-en.php#wb-auto-5').text
+soup = BeautifulSoup(html_text, 'lxml')
+table = soup.find('tbody')
+
+locations = table.find_all('a')
+locationsList = []
+for location in locations:
+    if location.text != "report":
+        locationsList.append(location.text)
+
+dates = table.find_all('span')
+datesList = []
+for date in dates:
+    if date.text != "Felt" and date.text != "Not felt":
+        datesList.append(date.text)
+
+print(locationsList)
+print(datesList)
