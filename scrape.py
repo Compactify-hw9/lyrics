0e311ba78c20f0f76ec5817fa8105131129089d3
user <aripoori111@gmail.com>
2022-11-19 21:25:42 -0500

    Long complete and mag complete

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 year = 2022
 originalURL = 'https://earthquakescanada.nrcan.gc.ca/recent/2022/index-en.php#wb-auto-5'
@@ -16,7 +17,11 @@ for location in locations:
     if location.text != "report":
         locationsList.append(location.text)
         longlist.append(location)
-    
+Magnitudes = []
+magnitudes = table.select("td:nth-child(3)")
+for magnitude in magnitudes:
+    Magnitudes.append(magnitude.text)
+print(Magnitudes)
 
 dates = table.find_all('span')
 datesList = []
@@ -30,8 +35,35 @@ for i in range (0,len(longlist)):
     longlist[i].replace(str(locationsList[i]),'')
     longlist[i]=longlist[i][:76]
 
-print(longlist)
 
-for i in range (0,len(longlist)):
-    html_text = requests.get(longlist[i]).text
-    soup = BeautifulSoup(html_text, 'lxml')
+
+Longitudes = []
+Latitudes = []
+for i in longlist:
+    html_text = requests.get(i)
+    soup = BeautifulSoup(html_text.text)
+    table = soup.select('table')
+    table = str(table)
+    x=table.find('headers="lat"')
+    Longitude = table[x+10:x+19]
+    number = re.findall("\d+\.\d+", Longitude)
+    if number == '':
+        Latitude = re.findall("[0-9]+",Latitude)
+        print(Latitude)
+    else:
+        Latitude = number
+    Longitudes.append(Longitude)
+
+    x=table.find('headers="lon"')
+    Latitude = table[x+10:x+19]
+    number = re.findall("\d+\.\d+", Latitude)
+    if number == []:
+        Latitude = re.findall("[0-9]+",Latitude)
+    else:
+        Latitude = number
+
+    Latitudes.append(Latitude)
+# print(Longitudes[0])
+# print(Latitudes[0])
+# print(locationsList[0])
+# print(dateslist[0])
